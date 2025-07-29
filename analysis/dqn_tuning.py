3ecc2c852cfc0e68abdc9e1d2dc4e48de7cd782a
Jeong Jinwoo <jwjeong96@gmail.com>
2025-07-29 12:35:10 +0900

    feat: tuning dqn_cnn_fd256_kernel3_1

--- analysis/dqn_tuning.py
+++ analysis/dqn_tuning.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     # visualize_test(model_name)
     pass
     # NOTE: No reward shaping, but bonus on reaching the end of the map
+    # TODO: exploration_final_eps to 0.01 or 0.0
+    # TODO: total_timesteps < 1_000_000
     # 
     # model_name = "dqn_cnn_fd256_kernel3_1" # similar to "dqn_cnn_group_fd256_gs_auto_1"
     # run_DQN_CnnPolicy(total_timesteps=1_000_000,
@@ -23,6 +25,7 @@ if __name__ == "__main__":
     #         learning_starts = 10_000,
     #         saved_model_name=model_name,
     #         tb_log_name="dqn_cnn_fd256_kernel3")
+    # test score: 352.5000
     # 
     # model_name = "dqn_cnn_fd512_kernel3_1"
     # run_DQN_CnnPolicy(total_timesteps=1_000_000,
@@ -99,6 +102,23 @@ if __name__ == "__main__":
 #         saved_model_name=model_name,
 #         tb_log_name="dqn_cnn_group_fd512_gs_auto")
 # test score: 355.0000
+
+# model_name = "dqn_cnn_fd256_kernel3_1" # similar to "dqn_cnn_group_fd256_gs_auto_1"
+# run_DQN_CnnPolicy(total_timesteps=1_000_000,
+#         features_dim=256, kernel_size=3,
+#         learning_rate=1e-4,
+#         exploration_initial_eps=1.0,
+#         exploration_fraction = 0.6,
+#         exploration_final_eps = 0.05,
+#         train_freq = (4, "episode"),
+#         gradient_steps = -1, # 1
+#         tau=1.0, # hard
+#         target_update_interval = 50,
+#         buffer_size = 10_000,
+#         batch_size=32,
+#         learning_starts = 10_000,
+#         saved_model_name=model_name,
+#         tb_log_name="dqn_cnn_fd256_kernel3")
 
 # saved model: dqn_cnn_fd256_kernel5_padding1.zip
 # log: dqn_cnn_group_fd512_kernel5_1
