6999aa8b14541c060b1905a2d35c79a11e8ff78d
Jeong Jinwoo <jwjeong96@gmail.com>
2025-08-18 17:49:20 +0900

    refactor: util for loading playlog

--- analysis/basic/main.py
+++ analysis/basic/main.py
@@ -1,49 +1,20 @@
-import json
-import numpy as np
-from pathlib import Path
+import sys
+import os
 
-EPISODE_METADATA = "episode_metadata.json"
-OBSERVATIONS = "observations.npy"
-PLAY_INFOS = "play_infos.npy"
-CAR_INFOS = "car_infos.npy"
-ACTIONS = "actions.npy"
-REWARDS = "rewards.npy"
-
-def data_dir(subjId):
-    current_file = Path(__file__).resolve()
-    current_dir = current_file.parent
-    parent_dir = current_dir.parent.parent
-    data_dir = parent_dir / "data" / str(subjId)
-    return data_dir
-
-def get_sorted_episodes(subj_data_dir):
-    episodes = []
-    for sub_dir in subj_data_dir.iterdir():
-        if sub_dir.is_dir():
-            episodes.append(sub_dir.name)
-    return sorted(episodes)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from analysis.util import data_dir, get_sorted_episodes, load_episode_play_log
 
 if __name__ == "__main__":
     subjId = 1
     subj_data_dir = data_dir(subjId)
     episodes = get_sorted_episodes(subj_data_dir)
     for episode in episodes:
-        episode_dir = subj_data_dir / episode
-        print(episode_dir)
-
-        with open(episode_dir / EPISODE_METADATA, "r") as f:
-            metadata = json.load(f)
-            print(metadata["road_metadata"][0])
-            print(metadata["car_metadata"][0])
-
-        observations = np.load(episode_dir / OBSERVATIONS)
-        play_infos   = np.load(episode_dir / PLAY_INFOS, allow_pickle=True)
-        car_infos    = np.load(episode_dir / CAR_INFOS, allow_pickle=True)
-        actions      = np.load(episode_dir / ACTIONS)
-        rewards      = np.load(episode_dir / REWARDS)
-
-        print(observations[0][0][0])
-        print(play_infos[0])
-        print(car_infos[0])
-        print(actions[0])
-        print(rewards[0])
+        play_log = load_episode_play_log(subj_data_dir, episode)
+        print(subj_data_dir / episode)
+        print(play_log["road_metadata"][0])
+        print(play_log["car_metadata"][0])
+        print(play_log["observations"][0][0][0])
+        print(play_log["play_infos"][0])
+        print(play_log["car_infos"][0])
+        print(play_log["actions"][0])
+        print(play_log["rewards"][0])
--- analysis/util.py
+++ analysis/util.py
@@ -1,4 +1,42 @@
+import json
+import numpy as np
+from pathlib import Path
+
 import matplotlib.pyplot as plt
+
+EPISODE_METADATA = "episode_metadata.json"
+OBSERVATIONS = "observations.npy"
+PLAY_INFOS = "play_infos.npy"
+CAR_INFOS = "car_infos.npy"
+ACTIONS = "actions.npy"
+REWARDS = "rewards.npy"
+
+def data_dir(subjId):
+    current_file = Path(__file__).resolve()
+    current_dir = current_file.parent
+    data_dir = current_dir.parent / "data" / str(subjId)
+    return data_dir
+
+def get_sorted_episodes(subj_data_dir):
+    episodes = []
+    for sub_dir in subj_data_dir.iterdir():
+        if sub_dir.is_dir():
+            episodes.append(sub_dir.name)
+    return sorted(episodes)
+
+def load_episode_play_log(subj_data_dir, episode):
+    episode_dir = subj_data_dir / episode
+    with open(episode_dir / EPISODE_METADATA, "r") as f:
+        metadata = json.load(f)
+    return {
+        "road_metadata": metadata["road_metadata"],
+        "car_metadata": metadata["car_metadata"],
+        "observations": np.load(episode_dir / OBSERVATIONS),
+        "play_infos": np.load(episode_dir / PLAY_INFOS, allow_pickle=True),
+        "car_infos": np.load(episode_dir / CAR_INFOS, allow_pickle=True),
+        "actions": np.load(episode_dir / ACTIONS),
+        "rewards": np.load(episode_dir / REWARDS),
+    }
 
 def save_plot(plot_func, filepath, title, xlabel, ylabel, xticks=None, legend=True):
     plt.figure(figsize=(10, 5))
