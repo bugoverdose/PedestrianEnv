b33ab6c983ec5000fbea4299dd39068a2f09a89b
Jeong Jinwoo <jwjeong96@gmail.com>
2025-08-19 15:58:57 +0900

    feat: implement FixedHorizonAbsorbIndicator wrapper

---
+++ analysis/irl/fixed_horizon.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+import gymnasium as gym
+
+# NOTE: Variable Horizon Environments Considered Harmful: https://imitation.readthedocs.io/en/latest/main-concepts/variable_horizon.html
+class FixedHorizonAbsorbIndicator(gym.Wrapper):
+    """
+    add Absorbing Indicator Channel (ongoing=0, absorbing=1)
+
+    add additional transitions until reaching `max_step`
+    - termination ~ max_step-1 : terminated=False, truncated=False
+    - max_step                 : terminated=False, truncated=True
+
+    add info["absorbing"]
+    - ~ termination          : info["absorbing"] = False
+    - termination ~ max_step : info["absorbing"] = True
+    """
+    def __init__(self, env, max_step: int):
+        super().__init__(env)
+        self.max_step = int(max_step)
+        self.cur_step = 0
+        self.is_absorbing = False
+
+        assert isinstance(env.observation_space, gym.spaces.Box)
+        C, Hh, Ww = env.observation_space.shape
+
+        # Box(C,H,W) -> Box(C+1,H,W)
+        low  = env.observation_space.low
+        high = env.observation_space.high
+        ind_low  = np.zeros((1, Hh, Ww), dtype=low.dtype)
+        ind_high = np.ones((1, Hh, Ww), dtype=high.dtype)
+        self.observation_space = gym.spaces.Box(
+            low=np.concatenate([low, ind_low], axis=0),
+            high=np.concatenate([high, ind_high], axis=0),
+            dtype=env.observation_space.dtype
+        )
+        self._absorbing_obs = np.zeros((C+1, Hh, Ww), dtype=self.observation_space.dtype)
+        self._absorbing_obs[-1, :, :] = 1.0
+
+    def ongoing_obs_with_absorbing_channel(self, obs):
+        assert obs.ndim == 3
+        ind = np.full((1, obs.shape[1], obs.shape[2]), float(0.0), dtype=obs.dtype)
+        return np.concatenate([obs, ind], axis=0)
+
+    def update_last_ongoing_info(self, info):
+        info = {} if info is None else dict(info)
+        info["absorbing"] = False
+        self.last_info = info
+
+    def reset(self, **kw):
+        self.cur_step = 0
+        self.is_absorbing = False
+        obs, info = self.env.reset(**kw)
+        self.update_last_ongoing_info(info)
+        return self.ongoing_obs_with_absorbing_channel(obs), self.last_info
+
+    def step(self, action):
+        self.cur_step += 1
+        if not self.is_absorbing:
+            obs, rew, terminated, truncated, info = self.env.step(action)
+            obs = self.ongoing_obs_with_absorbing_channel(obs)
+            self.update_last_ongoing_info(info)
+            if terminated or truncated:
+                self.is_absorbing = True # start absorbing after current transition
+        else:
+            obs = self._absorbing_obs
+            rew = 0.0
+            if not self.last_info["absorbing"]:
+                self.last_info["absorbing"] = True
+        return obs, rew, False, self.cur_step >= self.max_step, self.last_info
