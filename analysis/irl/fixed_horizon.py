9bd0a1c632645fc038b53535550dc71387a43235
Jeong Jinwoo <jwjeong96@gmail.com>
2025-08-19 17:24:46 +0900

    feat: implement create_fixed_horizon_TrajectoryWithRew

--- analysis/irl/fixed_horizon.py
+++ analysis/irl/fixed_horizon.py
@@ -2,6 +2,8 @@ import numpy as np
 
 import gymnasium as gym
 
+from imitation.data.types import TrajectoryWithRew
+
 # NOTE: Variable Horizon Environments Considered Harmful: https://imitation.readthedocs.io/en/latest/main-concepts/variable_horizon.html
 class FixedHorizonAbsorbIndicator(gym.Wrapper):
     """
@@ -37,11 +39,6 @@ class FixedHorizonAbsorbIndicator(gym.Wrapper):
         self._absorbing_obs = np.zeros((C+1, Hh, Ww), dtype=self.observation_space.dtype)
         self._absorbing_obs[-1, :, :] = 1.0
 
-    def ongoing_obs_with_absorbing_channel(self, obs):
-        assert obs.ndim == 3
-        ind = np.full((1, obs.shape[1], obs.shape[2]), float(0.0), dtype=obs.dtype)
-        return np.concatenate([obs, ind], axis=0)
-
     def update_last_ongoing_info(self, info):
         info = {} if info is None else dict(info)
         info["absorbing"] = False
@@ -52,13 +49,13 @@ class FixedHorizonAbsorbIndicator(gym.Wrapper):
         self.is_absorbing = False
         obs, info = self.env.reset(**kw)
         self.update_last_ongoing_info(info)
-        return self.ongoing_obs_with_absorbing_channel(obs), self.last_info
+        return _append_absorb_indicator(obs, 0.0), self.last_info
 
     def step(self, action):
         self.cur_step += 1
         if not self.is_absorbing:
             obs, rew, terminated, truncated, info = self.env.step(action)
-            obs = self.ongoing_obs_with_absorbing_channel(obs)
+            obs = _append_absorb_indicator(obs, 0.0)
             self.update_last_ongoing_info(info)
             if terminated or truncated:
                 self.is_absorbing = True # start absorbing after current transition
@@ -68,3 +65,44 @@ class FixedHorizonAbsorbIndicator(gym.Wrapper):
             if not self.last_info["absorbing"]:
                 self.last_info["absorbing"] = True
         return obs, rew, False, self.cur_step >= self.max_step, self.last_info
+
+NO_OP_ACTION = 0 # == Action.NOTHING.value
+
+def create_fixed_horizon_TrajectoryWithRew(
+        observations, # shape: (T+1,), dtype: float32
+        actions,      # shape: (T,), dtype: int64
+        rewards,      # shape: (T,), dtype: float32
+        infos,        # shape: (T,)
+        max_step
+    ):
+    T = len(actions)
+    # add Absorbing Indicator Channel 
+    obs = np.stack([_append_absorb_indicator(obs, 0.0) for obs in observations], axis=0)
+    pad_T = max_step - T
+    if pad_T > 0:
+        C,H,W = obs.shape[1:]
+        absorb = np.zeros((C,H,W), dtype=np.float32)
+        absorb[-1] = 1.0
+        obs_pad = np.stack([absorb]*pad_T, axis=0)   # (pad_T+1, C+1,H,W)
+        obs = np.concatenate([obs, obs_pad], axis=0) # (H+1, C+1,H,W)
+
+        # Do nothing and get no reward
+        actions_pad = np.full((pad_T,), NO_OP_ACTION, dtype=np.int64)
+        rewards_pad = np.zeros((pad_T,), dtype=np.float32)
+        actions = np.concatenate([actions, actions_pad], axis=0)
+        rewards = np.concatenate([rewards, rewards_pad], axis=0)
+
+        infos = list(infos) + [{} for _ in range(pad_T)]
+    return TrajectoryWithRew(
+        obs=obs,
+        acts=actions,
+        rews=rewards,
+        infos=infos,
+        terminal=True
+    )
+
+def _append_absorb_indicator(obs: np.ndarray, val: float):
+    assert obs.ndim == 3
+    C,H,W = obs.shape
+    ind = np.full((1,H,W), float(val), dtype=obs.dtype)
+    return np.concatenate([obs, ind], axis=0)
