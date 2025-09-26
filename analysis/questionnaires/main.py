e834589418be090d64a9bbb0873513ae3e1d0f94
Jeong Jinwoo <jwjeong96@gmail.com>
2025-09-26 17:08:56 +0900

    feat(analysis/questionnaires): calculate age, gender

--- analysis/questionnaires/main.py
+++ analysis/questionnaires/main.py
@@ -11,26 +11,44 @@ def preprocess(test = False):
     else:
         df = df[(df[RAW_SUBJECT_ID_COLNAME] >= 1000)]
 
-    base = {
-        "subj_id":     df[RAW_SUBJECT_ID_COLNAME].astype(int),
-        "gender":      df["demo_1"].astype(int), # NOTE: let's not debate about anything regarding this wording
-        "birth_year":  df["demo_2_1"].astype(int),
-        "birth_month": df["demo_2_2"].astype(int),
-        "school_year": df["demo_3_1"].astype(int),
-        "game_play_time": df["playtime_5"].astype(int),
-    }
-    bis     = {f"BIS_{i}":     df[f"BIS_{i+1}"].astype(int)     for i in range(30)}
-    stai_s  = {f"STAI-S_{i}":  df[f"STAI-S_{i+1}"].astype(int)  for i in range(20)}
-    stai_t  = {f"STAI-T_{i}":  df[f"STAI-T_{i+1}"].astype(int)  for i in range(20)}
-    dospert = {f"DOSPERT_{i}": df[f"DOSPERT_{i+1}"].astype(int) for i in range(30)}
-    cesd    = {f"CES-D_{i}":   df[f"CES-D_{i+1}"].astype(int)   for i in range(20)}
-    kigds   = {f"K-IGDS_{i}":  df[f"K-IGDS_{i+1}"].astype(int)  for i in range(27)}
-    kiat    = {f"K-IAT_{i}":   df[f"K-IAT_{i+1}"].astype(int)   for i in range(20)}
-    df = pd.DataFrame({**base, **bis, **stai_s, **stai_t, **dospert, **cesd, **kigds, **kiat})
+    def expand(questionnaire_name, question_count):
+        return {f"raw_{questionnaire_name}_{i+1}": df[f"{questionnaire_name}_{i+1}"].astype(int) for i in range(question_count)}
+    df = pd.DataFrame({
+        "subj_id":         df[RAW_SUBJECT_ID_COLNAME].astype(int),
+        "age":             _calculate_age(df),
+        "gender":          df["demo_1"].astype(int), # NOTE: let's not debate about anything regarding this wording
+        "school_year":     df["demo_3_1"].astype(int),
+
+        "game_play_time":  df["playtime_5"].astype(int),
+        **expand("BIS", 30),
+        **expand("STAI-S", 20),
+        **expand("STAI-T", 20),
+        **expand("DOSPERT", 30),
+        **expand("CES-D", 20),
+        **expand("K-IGDS", 27),
+        **expand("K-IAT", 20),
+    })
     print(df)
-    print(f"Total Subjects: {len(df)} ({df['subj_id'].tolist()})")
     return df
 
+def _calculate_age(df):
+    response_date = pd.to_datetime(df["EndDate"], errors='coerce') # format: 2025-09-26 14:38:19
+    response_year = response_date.dt.year.astype(int)
+    response_month = response_date.dt.month.astype(int)
+    birth_year = df["demo_2_1"].astype(int)
+    birth_month = df["demo_2_2"].astype(int)
+    age = response_year - birth_year
+    age[response_month < birth_month] -= 1
+    return age.astype(int)
+
+def demographics(df):
+    print(f"Total Subjects: {len(df)} ({df['subj_id'].tolist()})")
+    age = df["age"]
+    print(f"Age: mean = {age.mean()} ({age.min()} ~ {age.max()})")
+    gender = df["gender"]
+    print(f"Gender: {len(df[gender == 2])} males & {len(df[gender == 1])} females")
+
 if __name__ == "__main__":
-    # preprocess()
-    preprocess(test = True)
+    # df = preprocess()
+    df = preprocess(test = True)
+    demographics(df)
