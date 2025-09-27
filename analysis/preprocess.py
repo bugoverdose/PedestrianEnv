921f86ade882018c4b812032a712c1744f2f9129
Jeong Jinwoo <jwjeong96@gmail.com>
2025-09-27 21:22:52 +0900

    refactor(analysis): separate data preprocessing from jupyter notebook

---
+++ analysis/preprocess.py
@@ -0,0 +1,17 @@
+from questionnaires import load_survey_results
+from pedestrian_stats import build_pedestrian_task_results, crosswalk_statistics
+from behavior_tasks import build_behavioral_task_results
+
+DATAFILE = "data.csv"
+
+if __name__ == "__main__":
+    df = load_survey_results(test = True)
+    # TODO: 데이터 부족으로 인한 임시방편. 향후 현실적인 데이터로 대체.
+    df.loc[3, "subj_id"] = 502
+    df.loc[4, "subj_id"] = 503
+    subj_ids = df["subj_id"].to_list()
+
+    df = build_pedestrian_task_results(df, subj_ids)
+    df = build_behavioral_task_results(df, subj_ids)
+
+    df.to_csv(DATAFILE, index=False)
--- analysis/util.py
+++ analysis/util.py
@@ -52,3 +52,28 @@ def load_episode_full_log(subj_data_dir, episode):
 #         plt.show()
 #     else:
 #         plt.savefig(filepath, bbox_inches='tight')
+
+if __name__ == "__main__":
+    subjId = 100
+    subj_data_dir = data_dir(subjId)
+    episodes = get_sorted_episodes(subj_data_dir)
+    for episode in episodes:
+        full_log = load_episode_full_log(subj_data_dir, episode)
+        print()
+        print(subj_data_dir / episode)
+        print("env_configuration")
+        print(full_log["episode_metadata"]["env_configuration"])
+        print("episode_configuration")
+        print(full_log["episode_metadata"]["episode_configuration"])
+        print("play_infos")
+        print(full_log["play_infos"][0])
+        print("observations")
+        print(full_log["observations"][0])
+        print("observation size:", len(full_log["observations"][0]))
+        print("actions:", full_log["actions"][:10])
+        print("rewards:", full_log["rewards"][:10])
+        assert type(full_log["rewards"][0]) == np.float32 or type(full_log["rewards"][0]) == np.float64
+        print("trajectory size:", len(full_log["actions"]))
+        assert len(full_log["play_infos"]) == len(full_log["observations"])
+        assert len(full_log["actions"]) == len(full_log["rewards"])
+        assert len(full_log["observations"]) == len(full_log["actions"]) + 1
